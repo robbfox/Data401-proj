d089605e9ca84c64ab5cf99ef6d68367d4dc85fc
Bluna1311 <36048735+Bluna1311@users.noreply.github.com>
2024-04-09 16:02:41 +0100

    Delete Classes directory
    Remove classes till dev

--- Classes/JSON_extractor.py
+++
@@ -1,6 +0,0 @@
-from data_extractor import DataExtractor
-import pandas as pd
-
-class JSONExtractor(DataExtractor):
-    def extract(self, file_path):
-        return pd.read_json(file_path)
--- Classes/data_extractor.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-class DataExtractor:
-    def extract(self, file_path):
-        raise NotImplementedError("This method should be overridden by subclasses.")
