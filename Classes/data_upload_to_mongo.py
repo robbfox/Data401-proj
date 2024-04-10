5b53a9dcf12735f4d02cb26bced07a07e3bc4537
Brian Luna <Brian1311@hotmail.co.uk>
2024-04-10 17:32:58 +0100

    Classes for upload to mongo fixed. all academy data going through. duplicated removed

---
+++ Classes/data_upload_to_mongo.py
@@ -0,0 +1,74 @@
+from mongo_atlas_connect import *
+import json
+import pandas as pd
+
+class MongoUploader():
+    def format_identifier(self, data_input, collection):
+        file_type = type(data_input)
+
+
+        if collection == "Academy/":
+            self.academy_dataframe_uploader(data_input)
+
+        elif collection == "Talent/":
+            if file_type == str:
+                print("In JSON")
+                self.json_uploader(data_input)
+            else:
+                print("In dataframe")
+                self.talent_dataframe_uploader(data_input)
+
+    def json_uploader(self, data_input):
+        print("In JSON uploader")
+        if isinstance(data_input, str):
+            # If the input data is a string, attempt to parse it as JSON
+            try:
+                parsed_data = json.loads(data_input)
+            except json.JSONDecodeError as e:
+                print(f"Error parsing input data as JSON: {e}")
+                return
+        else:
+            parsed_data = data_input
+
+        # Check if parsed_data is a dictionary or a list of dictionaries
+        if isinstance(parsed_data, dict):
+            # If it's a single dictionary, insert it directly
+            db.Talent.insert_one(parsed_data)
+            print("Successfully uploaded in Talent")
+        elif isinstance(parsed_data, list) and all(isinstance(item, dict) for item in parsed_data):
+            # If it's a list of dictionaries, insert each dictionary separately
+            db.Talent.insert_many(parsed_data)
+            print("Successfully uploaded multiple documents in Talent")
+        else:
+            print("Error: input data is not a valid dictionary or a list of dictionaries")
+
+    def talent_dataframe_uploader(self, data_input):
+        # Assuming data_input is a single dict or a DataFrame with a single row
+        if isinstance(data_input, pd.DataFrame):
+            # Convert DataFrame to a single dict
+            dict_input_talent = data_input.to_dict(orient='records')[0]
+        elif isinstance(data_input, dict):
+            # Use data_input directly if it's already a dict
+            dict_input_talent = data_input
+        else:
+            raise ValueError("data_input must be a DataFrame or a dict")
+
+        # Insert a single document into the Talent collection
+        db.Talent.insert_one(dict_input_talent)
+        print("Successfully uploaded data to Talent collection.")
+    def academy_dataframe_uploader(self, data_input):
+        # Assuming data_input is a DataFrame with a single row
+        if isinstance(data_input, pd.DataFrame):
+            # Convert DataFrame to a single dict
+            dict_input_academy = data_input.to_dict(orient='records')[0]
+        else:
+            raise ValueError("data_input must be a DataFrame with a single row")
+
+        # Insert a single document into the Academy collection
+        db.Academy.insert_one(dict_input_academy)
+        print("Successfully uploaded in Academy")
+
+
+
+
+
--- Runner.py
+++ Runner.py
@@ -2,22 +2,37 @@ from Classes.TXT_extractor import TXTExtractor
 from Classes.CSV_extractor import CSVExtractor
 from Classes.s3_manager import S3Manager
 from Classes.JSON_extractor import JSONExtractor
+from Classes.data_upload_to_mongo import MongoUploader
 
 
 bucket_name = "data-eng-401-final-project"
 s3 = S3Manager(bucket_name)
-files_to_process = s3.list_files("Talent/")
-for filename in files_to_process:
-    file = s3.read_file_to_memory(filename)
-    if filename.endswith(".csv"):
-        file = CSVExtractor().extract(file)
-        print(file)
-    elif filename.endswith(".txt"):
-        file = TXTExtractor().extract(file)
-        print(file)
-        print("newFile")
-    elif filename.endswith(".json"):
-        file = JSONExtractor().extract(file)
+S3col= ["Talent/", "Academy/"]
+loader = MongoUploader()
+
+# files_to_process = s3.list_files("Talent/")
+
+for col in S3col:
+    files_to_process = s3.list_files(col)
+    files_to_process = list(set(s3.list_files(col)))
+    print(len(files_to_process))
+
+    for filename in files_to_process:
+        file = s3.read_file_to_memory(filename)
+        if filename.endswith(".csv"):
+            file = CSVExtractor().extract(file)
+            print(file)
+            loader.format_identifier(file, col)
+        elif filename.endswith(".txt"):
+            file = TXTExtractor().extract(file)
+            print(file)
+            loader.format_identifier(file, col)
+        elif filename.endswith(".json"):
+            pass
+            #file = JSONExtractor().extract(file)
+            #loader.format_identifier(file, col)
+
+
 
 
 
