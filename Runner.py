647a337707e9ea23d46a449ab0c37c185a5fa99a
Brian Luna <Brian1311@hotmail.co.uk>
2024-04-11 20:12:23 +0100

    proccessedFiles class just has a reader and writer to store files names. No security as it only holds names of files and to access anything you would need the connection for the bucket, file and config. CSV extract test fixed as it was having format issues.

--- Runner.py
+++ Runner.py
@@ -3,23 +3,28 @@ from Classes.CSV_extractor import CSVExtractor
 from Classes.s3_manager import S3Manager
 from Classes.JSON_extractor import JSONExtractor
 from Classes.data_upload_to_mongo import MongoUploader
+from Classes.ProcessedFiles import ProcessedFilesManager
 
 
 bucket_name = "data-eng-401-final-project"
 s3 = S3Manager(bucket_name)
 S3col= ["Talent/", "Academy/"]
-
+processed_files_path = 'processed_files.txt'
 loader = MongoUploader()
+filehandler = ProcessedFilesManager(processed_files_path)
 
+processed_files_path = './processed_files.txt'
+processed_files = filehandler.read_processed_files_list()
 # files_to_process = s3.list_files("Talent/")
 
 for col in S3col:
     files_to_process = s3.list_files(col)
     files_to_process = list(set(s3.list_files(col)))
+    new_files = [f for f in files_to_process if f not in processed_files]
 
-    print(len(files_to_process))
+    print(len(new_files))
 
-    for filename in files_to_process:
+    for filename in new_files:
         file = s3.read_file_to_memory(filename)
         if filename.endswith(".csv"):
             file = CSVExtractor().extract(file,filename)
@@ -33,6 +38,7 @@ for col in S3col:
             file = JSONExtractor().extract(file)
             print(filename)
             loader.format_identifier(file, col,filename)
+        filehandler.write_processed_file(filename)
 
 
 
