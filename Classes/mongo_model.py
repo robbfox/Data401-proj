ec7363c1cadbf65832982177cde3d0aec09be312
Usama Asghar <usama.asghar@hotmail.co.uk>
2024-04-11 16:04:23 +0100

    class working with all data

--- Classes/mongo_model.py
+++ Classes/mongo_model.py
@@ -7,10 +7,8 @@ class CreateCandidate:
         regex_pattern = re.compile(r".json$")
         file_origin = db.Talent.find_one({"name": input_name, "date": input_date, "File origin": {"$regex": regex_pattern}},
          {"File origin": 1, "_id": 0})
-        print(file_origin)
         file_name = file_origin["File origin"]
         candidate_id = file_name.split('/')[1].split('.')[0]
-        print(candidate_id)
         self.insert_candidate_id(candidate_id, input_name)
         self.retrieve_candidate_info(candidate_id, input_name)
 
@@ -21,6 +19,7 @@ class CreateCandidate:
         db.Academy.update_many(
             {"name": input_name},
             {"$set": {"candidate_id": candidate_id}})
+
     def retrieve_candidate_info(self, candidate_id, input_name):
         regex_pattern_json = re.compile(r".json$")
         details_json = db.Talent.find_one(
@@ -41,6 +40,7 @@ class CreateCandidate:
 
         self.final_insert_into_candidates(details_json, personal_info_csv,
                                sparta_day_txt, academy_scores_csv, input_name, candidate_id)
+
     def final_insert_into_candidates(self, details_json, personal_info_csv,
                                sparta_day_txt, academy_scores_csv, input_name, candidate_id):
 
@@ -53,9 +53,12 @@ class CreateCandidate:
             "personal_info": personal_info_csv
         }
 
-        db.Candidates.insert_one(data)
+        result = db.Candidates.find_one({"_id": candidate_id}, {})
+        if result is None:
+            db.Candidates.insert_one(data)
 
 
 # name_id = CreateCandidate()
 # name_id.get_id_talent('Edvard Northen', '07/08/2019')
 # name_id.get_id_talent('Hilary Willmore', '01/08/2019')
+#
